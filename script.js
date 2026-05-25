9c5946be4da21a211b4dce47fe168e8f86eff5c3
Matheus Silva Araujo <araujotheusti@gmail.com>
2026-05-25 00:40:43 -0300

    quarto commit js

--- script.js
+++ script.js
@@ -1,45 +1,42 @@
 let conta = 0
 let pessoas = 0
+let porcentagem = 0
 
-
-const contaInput = document.querySelector("conta")
-contaInput.addEventListener("input, receberValorConta")
+const contaInput = document.querySelector("#conta")
+contaInput.addEventListener("input", receberValorConta)
 
 function receberValorConta(evento) {
     conta = Number(evento.target.value)
-
 }
 
 const pessoasInput = document.querySelector("#pessoas")
 pessoasInput.addEventListener("input", receberQuantidadePessoas)
 
 function receberQuantidadePessoas(evento) {
-    const paragrafoErro = document.querySelector(".pessoas #erro")
-    const divErro = document.querySelector(".pessoas .input-box")
-
-    if(evento.target.value === "0"){
-        paragrafoErro.computedStyleMap.display = "block"
-        divErro.setAttribute("id", "erro-div")
-    } else {
-        paragrafoErro.computedStyleMap.display = "none"
-        divErro.setAttribute("id", "")
-        pessoas = Number(evento.target.value)
-    }
 }
 
-const botoesGorgeta = document.querySelectorAll(".gorjeta input[type='button']")
+const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
-    botao.addEventListener("click", receberPorcentagemBotao)
+    botao.addEventListener("click", receberPorcentagem)
 })
 
-function receberPorcentagemBotao(evento) {
+function receberPorcentagem(evento) {
     botoesGorjeta.forEach(botao => {
-        botao.classList.add("botao-ativo")
+        botao.classList.remove("botao-ativo")
 
         if(botao.value === evento.target.value) {
             botao.classList.add("botao-ativo")
         }
     })
 
-    porcentagem = parseFloat(evento.target.value) / 100
+    if(evento.target.value !== "") {
+        porcentagem = parseFloat(evento.target.value) / 100
+    } else {
+        porcentagem = 0
+    }
+
+    console.log(porcentagem)
 }
+
+const gorjetaInput = document.querySelector("#outra")
+gorjetaInput.addEventListener("input", receberPorcentagem)
